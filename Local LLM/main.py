95327b6d2c010f98729b85db3147769c67e7bdd3
Nazlı As <f.nazlias@gmail.com>
2024-12-01 22:07:55 +0300

    Initial commit

---
+++ Local LLM/main.py
@@ -0,0 +1,38 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+
+template = """
+Answer the question below.
+
+
+Here is the conversation history: {context}
+
+Question:{question}
+
+Answer:
+
+
+"""
+
+
+model = OllamaLLM(model="llama3.1")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+
+def handle_conversation():
+    context = ""
+    print("welcome to ai chatbot! type 'exit' to quit.")
+    while True:
+        user_input = input("you: ")
+        if user_input.lower() == "exit":
+            break
+        result = chain.invoke({"context": context, "question": user_input})
+
+        print("bot: ", result)
+        context += f"\nUser: {user_input}\nAI: {result}"
+
+
+if __name__ == "__main__":
+    handle_conversation()
